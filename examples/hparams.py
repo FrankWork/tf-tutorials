ab27c61dbd014b6a0d22abbd4fc89d461a30becc
FrankWork <lzh00776@163.com>
2017-11-24 11:59:53 +0800

    hparams

---
+++ examples/hparams.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('a_param', 'in a', 'Which dataset to generate data for')
+
+hparams = tf.contrib.training.HParams(
+                                learning_rate=0.1, 
+                                num_hidden_units=100,
+                                activations=['relu', 'tanh'])
+
+print(hparams.learning_rate)
+
+hparams.add_hparam('rnn_size', 110)
+
+print(hparams.rnn_size)
+
+def main(_):
+  if FLAGS.a_param:
+    print(FLAGS.a_param)
+
+if __name__ == '__main__':
+  tf.app.run()
